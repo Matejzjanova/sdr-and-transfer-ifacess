03cc34505a308eca0dfb77d22366c9e096bd08b4
mihsha <mzlobins@yandex.ru>
2025-04-14 18:11:19 +0300

    try to add rx/tx

--- HackRFControl.cpp
+++ HackRFControl.cpp
@@ -4,40 +4,50 @@
 using namespace std;
 
 #include <iostream>
+#include <future>
 #include "IRFControl.h"
 #include "libhackrf/src/hackrf.h"
 class HackRFControl: IRFControl {
     HackRFControl() {
-        if (hackrf_init()) {
-            if(hackrf_open(&device)) {
-                //information about opened hackrf version etc
-            };
-        };
+        errorHandler(hackrf_init());
+        errorHandler(hackrf_open(&device));
     }
 
-    virtual uint32_t setFrequency( uint64_t freq) {
-
-        uint32_t result = hackrf_set_freq(device, freq);
-        return HACKRF_ERROR_OTHER;
+    static void errorHandler(int code) {
+        switch(code) {
+            case HACKRF_SUCCESS:
+                return;
+            default:
+                throw std::runtime_error("HACKRF ERROR:" + string(hackrf_error_name( (hackrf_error) code)));
+        }
     }
 
-    virtual uint64_t setFrequency(uint64_t from, uint64_t to, rf_path_filter path) {
-        if (hackrf_set_freq_explicit(device, from, to, path)) {
-            return 0;
-        };
-        return HACKRF_ERROR_OTHER;
+    void setFrequency( uint64_t freq) override {
+        errorHandler(hackrf_set_freq(device, freq));
     }
 
-    virtual uint32_t setAtt() {
-
+    uint64_t setFrequency(uint64_t from, uint64_t to, rf_path_filter path) {
+        errorHandler(hackrf_set_freq_explicit(device, from, to, path));
     }
 
-    virtual uint32_t setLNA(uint32_t gain)  {
-        if(hackrf_set_lna_gain(device, gain)) {
-            return 0;
-        };
-        return HACKRF_ERROR_OTHER;
+    void setLNA(uint32_t gain) override {
+        errorHandler(hackrf_set_lna_gain(device, gain));
     }
 
-    hackrf_device* device{};
+    uint32_t recieve(void *buf, size_t size) override {
+        hackrf_transfer rx;
+        std::future<void*> buff = async(hackrf_start_rx(device, manager, rx.rx_ctx));
+    }
+
+    uint32_t send(void *buf, size_t size) override {
+
+    }
+    hackrf_device* device;
+
+    void *manager = nullptr;
+
+    ~HackRFControl(){
+        hackrf_close(device);
+    };
+
 };
--- IRFControl.h
+++ IRFControl.h
@@ -10,9 +10,9 @@
 
 class IRFControl {
 //    Control methods
-    virtual uint32_t setFrequency(uint64_t freq) = 0;
-    virtual uint32_t setLNA(uint32_t gain) = 0;
-    virtual  uint32_t setAtt() = 0;
+    virtual void setFrequency(uint64_t freq) = 0;
+    virtual void setLNA(uint32_t gain) = 0;
+    virtual  void setAtt() = 0;
 //
 //    virtual uint32_t findDevice() = 0;
 //    virtual uint32_t init() = 0; //constructor
@@ -20,7 +20,10 @@ class IRFControl {
 //RX/TX
     virtual uint32_t send(void* buf, size_t size) = 0;
     virtual uint32_t recieve(void* buf, size_t size) = 0;
-};
 
+    virtual void* allocateBuf(size_t size) = 0;
+    virtual void setIRQsize(size_t size) = 0;
+    virtual void setIRQManager(void(*foo)(void *), void * buff) = 0;
+};
 
 #endif //RFI_BASE_ON_LIBHACKRF_IRFCONTROL_H
